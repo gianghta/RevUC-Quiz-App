fefacfc3694d98814b94d35dbc426135f522abdf
baohuynhlam <shinsusenju@aol.com>
2020-02-23 00:33:11 -0500

    Split questions into 4 multiple buttons

--- app.py
+++ app.py
@@ -7,9 +7,19 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     number = random.randint(0, 2)
+
+    # Get question data
     question = get_question("questions.txt", number)
-    question_html = change_to_html(question)
-    return render_template('index.html', question = question_html)
+
+    # Split question data into variables to add to 4 answer buttons
+    # Question data format: [country], [correct_answer]*[choice1], [chioce2], [choice3], [choice4],
+    splitted = question.split("*")
+    country, correct_city = splitted[0].split(",")
+    choices = splitted[1].strip().split(",")[0:-1]
+    
+
+    # Display question and 4 answer choice   
+    return render_template('index.html', country = country, choices = choices)
 
 
 @app.route('/play')
--- random_quiz.py
+++ random_quiz.py
@@ -19,11 +19,16 @@ def generate_test(data_path, result_path, num_choice):
 
     for country in capitals.keys():
 
-        # Print answer header
-        questions.write(rf"What is the capital of {country}?\n")
+        # Write answer header
+        questions.write(rf"{country},")
 
-        # Choose correct answer
+        # Write correct answer
         correct_city = capitals[country]
+        questions.write(f"{correct_city}")
+
+        # Separate answer part and question part by "*"
+        # Format: [country], [correct_answer] * [choice1], [chioce2], [choice3], [choice4]
+        questions.write("*")
 
         # Choose additional 3 wrong answer
         wrong_cities = cities.copy()
@@ -39,10 +44,11 @@ def generate_test(data_path, result_path, num_choice):
 
         # Print out multiple choices:
         for choice in ans_list:
-            questions.write(rf"{choice}\n")
+            questions.write(rf"{choice},")
 
         questions.write("\n")
 
+        
     # Closing files
     test_data.close()
     questions.close()
@@ -53,10 +59,25 @@ def change_to_html(text):
     return text
 
 
+def check_and_continue(choice, correct_choice):
+    return choice == correct_choice
+
+
+
 def get_question(questions_bank_path, question_num):
     with open(questions_bank_path) as questions:
         return questions.readlines()[question_num]
 
 if __name__ == "__main__":
-    generate_test("nations-and-capitals.txt", "questions.txt", num_choice=4)
+    number = 1
+
+    # Get question data
+    question = get_question("questions.txt", number)
+
+    # Split question data into variables to add to 4 answer buttons
+    # Question data format: [country], [correct_answer]*[choice1], [chioce2], [choice3], [choice4],
+    splitted = question.split("*")
+    country, correct_city = splitted[0].split(",")
+    choices = splitted[1].strip().split(",")[0:-1]
+    print(correct_city)
     
